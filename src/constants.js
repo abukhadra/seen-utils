2a0f8602b0ac0236e86a0cf0e082c96f90868b27
Mustafa Abukhadra <abukhadra@gmail.com>
2024-09-29 12:46:43 +0300

    update

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
+import {regexp} from './util.js'
 const MASHRIQ_DIGIT = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'] 
 const MAGHRIB_DIGIT = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
 const TATWEEL = '\u{640}' 
