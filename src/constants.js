714712f7284a57afc72b194099147efb343fece5
Mustafa Abukhadra <abukhadra@gmail.com>
2024-09-29 11:57:59 +0300

    update

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const MASHRIQ_DIGIT = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'] 
+export const MAGHRIB_DIGIT = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
+export const TATWEEL = '\u{640}' 
+export const ANY_LETTER = regexp("\\p{L}") 
+export const ANY_NUM = regexp("\\p{N}") 
--- src/util.js
+++ src/util.js
@@ -1,8 +1,8 @@
-const MASHRIQ_DIGIT = ['٠', '١', '٢', '٣', '٤', '٥', '٦', '٧', '٨', '٩'] 
-const MAGHRIB_DIGIT = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] 
-const TATWEEL = '\u{640}' 
-const ANY_LETTER = regexp("\\p{L}") 
-const ANY_NUM = regexp("\\p{N}") 
+import {
+    MASHRIQ_DIGIT,
+    MAGHRIB_DIGIT,
+
+}  from './constants.js'
 
 function is_mashriq_num(n) { return contains(MASHRIQ_DIGIT, n[0]) }
 
